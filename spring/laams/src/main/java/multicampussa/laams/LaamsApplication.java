a14b643d4c7030211f048d434dfa2c7979ab7504
장성운 <sungoon0646@gmail.com>
2023-10-16 13:09:39 +0900

    init: spring init

---
+++ spring/laams/src/main/java/multicampussa/laams/LaamsApplication.java
@@ -0,0 +1,13 @@
+package multicampussa.laams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaamsApplication.class, args);
+	}
+
+}
