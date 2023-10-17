8798fed463ad3a635110a0e96635927e5bbb0b09
박단비 <qkreksqhd@naver.com>
2023-10-17 15:54:00 +0900

    Feat: Notice Entity 생성

--- spring/laams/src/main/java/multicampussa/laams/home/notice/domain/Notice.java
+++ spring/laams/src/main/java/multicampussa/laams/home/notice/domain/Notice.java
@@ -1,4 +1,31 @@
 package multicampussa.laams.home.notice.domain;
 
-public class Notice {
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import multicampussa.laams.global.BaseTimeEntity;
+import multicampussa.laams.manager.domain.examinee.Examinee;
+import org.apache.catalina.User;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Notice extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long no;
+
+    private String title;
+
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="examinee_no")
+    private Examinee user;
+
+
+
 }
