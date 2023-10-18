1b5ecc7f09951f96c3130d68f83f3c5b102c355b
김현지 <hjkasd12345@naver.com>
2023-10-18 17:43:24 +0900

    Feat: 감독관 시험 목록 조회 기능

---
+++ spring/laams/src/main/java/multicampussa/laams/director/service/DirectorService.java
@@ -0,0 +1,32 @@
+package multicampussa.laams.director.service;
+
+import lombok.RequiredArgsConstructor;
+import multicampussa.laams.director.dto.ExamListDto;
+import multicampussa.laams.manager.domain.exam.Exam;
+import multicampussa.laams.manager.domain.exam.ExamManagerRepository;
+import multicampussa.laams.manager.domain.exam.ExamRepository;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class DirectorService {
+
+    private final ExamRepository examRepository;
+    private final ExamManagerRepository examManagerRepository;
+
+    @Transactional
+    public List<ExamListDto> getExamList(Long directorNo) {
+        List<ExamListDto> examListDtos = new ArrayList<>();
+        List<Exam> exams = examManagerRepository.findAllByDirectorNo(directorNo);
+
+        for(Exam exam : exams){
+            examListDtos.add(new ExamListDto(exam));
+        }
+        return examListDtos;
+
+    }
+}
--- spring/laams/src/main/java/multicampussa/laams/manager/domain/exam/ExamRepository.java
+++ spring/laams/src/main/java/multicampussa/laams/manager/domain/exam/ExamRepository.java
@@ -2,7 +2,7 @@ package multicampussa.laams.manager.domain.exam;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
 
 public interface ExamRepository extends JpaRepository<Exam, Long> {
+
 }
