97b750e5c5ff68670aff8d9bfac0bf619bbddf62
Going9 <ickdor123@gmail.com>
2023-10-20 17:27:44 +0900

    Feat: 시험 상세 조회 DTO, 감독 리스트 DTO 완성

---
+++ spring/laams/src/main/java/multicampussa/laams/manager/dto/director/DirectorListResponse.java
@@ -0,0 +1,9 @@
+package multicampussa.laams.manager.dto.director;
+
+import lombok.Getter;
+
+@Getter
+public class DirectorListResponse {
+    private String directorName;
+    private String directorPhoneNum;
+}
