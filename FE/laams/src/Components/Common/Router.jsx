5dcf31bca15e34c6f8905723e383e19e902de4a8
한승현 <vkflek1232@naver.com>
2023-10-23 13:23:25 +0900

    Feat : 헤더 PC UI 완성 - 홈 로고 추가 - 메뉴 컴포넌트 추가 - FIXME - 메뉴링크 연결 - 벨기능 연결 - 모바일 미디어 쿼리 처리

--- FE/laams/src/Components/Common/Router.jsx
+++ FE/laams/src/Components/Common/Router.jsx
@@ -1,12 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from "../Home/Home.jsx"
+import Header from './Header.jsx';
 
 const Router = () => {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<><Header/><Home /></>} />
         </Routes>
       </BrowserRouter>
     </>
