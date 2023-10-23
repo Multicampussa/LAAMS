4dcf4e6d7948e284c305a8a1166348fb4120ef05
한승현 <vkflek1232@naver.com>
2023-10-23 16:14:45 +0900

    Feat : 모달 기초 설정 - 추가   - 모달 관련 redux 추가   - 모달 관련 scss파일 추가 - 변경   - 모달 관련 redux 설정 변경   - mediaQuery 수정   - reset style 오류 수정

--- FE/laams/src/redux/reducer/index.js
+++ FE/laams/src/redux/reducer/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from "redux";
 import User from "./userReducer";
-
+import Modal from "./modalReducer"
 const rootReducer = combineReducers({
   User,
+  Modal,
 });
 export default rootReducer;
