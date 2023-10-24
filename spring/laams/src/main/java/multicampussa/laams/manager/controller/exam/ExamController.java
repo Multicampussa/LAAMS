b1255b42d85534abb30c7be4623829d09260e8e7
Going9 <ickdor123@gmail.com>
2023-10-24 15:32:30 +0900

    Feat: 스웨거 테그 입력

--- spring/laams/src/main/java/multicampussa/laams/manager/controller/exam/ExamController.java
+++ spring/laams/src/main/java/multicampussa/laams/manager/controller/exam/ExamController.java
@@ -1,5 +1,7 @@
 package multicampussa.laams.manager.controller.exam;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import multicampussa.laams.home.member.jwt.JwtTokenProvider;
 import multicampussa.laams.manager.dto.exam.request.ExamCreateRequest;
 import multicampussa.laams.manager.dto.exam.request.ExamUpdateRequest;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Api(tags = "시험 관련")
 @RestController
 public class ExamController {
 
@@ -22,30 +25,35 @@ public class ExamController {
     }
 
     // 시험 생성
+    @ApiOperation("시험 생성")
     @PostMapping("/manager/exam")
     public void saveExam(@RequestBody ExamCreateRequest request) {
         examService.saveExam(request);
     }
 
     // 시험 목록 조회
+    @ApiOperation("시험 목록 조회")
     @GetMapping("/manager/exams")
     public List<ExamResponse> getExams() {
         return examService.getExams();
     }
 
     // 시험 상세 조회
+    @ApiOperation("시험 상세 조회")
     @GetMapping("/manager/exam/{no}")
     public ExamDetailResponse getExam(@PathVariable Long no) {
         return examService.getExam(no);
     }
 
     // 시험 수정
+    @ApiOperation("시험 수정")
     @PutMapping("/manager/exam")
     public void updateExam(@RequestBody ExamUpdateRequest request) {
         examService.updateExam(request);
     }
 
     // 시험 삭제
+    @ApiOperation("시험 삭제")
     @DeleteMapping("/manager/exam/{no}")
     public void deleteExam(@PathVariable Long no) {
         examService.deleteExam(no);
