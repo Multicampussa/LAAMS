d01454e0acc5cd8e57fe7dd5496c823f04acaca7
한승현 <vkflek1232@naver.com>
2023-10-24 15:19:22 +0900

    Feat 운영자 홈 레이아웃 (PC & 모바일)

--- FE/laams/src/Components/Manager/Home/ManagerHome.jsx
+++ FE/laams/src/Components/Manager/Home/ManagerHome.jsx
@@ -1,8 +1,27 @@
-import React, { useCallback, useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useMemo, useState } from 'react'
 
 const ManagerHome = () => {
   const [page,setPage]=useState(1);
   const [title,setTitle] = useState("시험일정");
+  const [chartTitle,setChartTitle] = useState("센터 별 시험 횟수");
+  const [selectOpen,setSelectOpen] = useState(false);
+
+  const handleChartItem = useCallback((title)=>{
+    setChartTitle(title);
+    setSelectOpen(false);
+  },[]);
+  const chartItem = useMemo(()=>{
+    const chartList = [
+      "센터 별 시험 횟수",
+      "센터 별 보상 건 수",
+      "센터 별 응시자 수",
+      "일년간 에러리포트 수 변화",
+      "일년간 보상요청 수 변화"
+    ]
+    return chartList.map((e,idx)=>
+    <li className='manager-home-chart-item' key={idx} onClick={()=>handleChartItem(e)}>{e}</li>);
+  },[]);
+
   const sliderPrev = useCallback(()=>{
     if(page === 1) return;
     setPage(page-1);
@@ -35,22 +54,59 @@ const ManagerHome = () => {
   return (
     <section className='manager-home'>
       <div className='manager-home-title'>
-        <button onClick={sliderPrev}>이전</button>
+        <button onClick={sliderPrev}>{"<"}</button>
         <div>{title}</div>
-        <button onClick={sliderNext}>다음</button>
+        <button onClick={sliderNext}>{">"}</button>
       </div>
       <ul className={`manager-home-slider-${page}`}>
-        <li className='manager-home-schedule'>
-          
+        <li className='manager-home-box'>
+          <div className='manager-home-box-title'>오늘 일정</div>
+          <ul>
+            <li>
+              구미 센터점 09:00 - 10:00
+            </li>
+            <li>
+              구미 센터점 09:00 - 10:00
+            </li>
+            <li>
+              구미 센터점 09:00 - 10:00
+            </li>
+            <li>
+              구미 센터점 09:00 - 10:00
+            </li>
+          </ul>
         </li>
-        <li className='manager-home-reward'>
-
+        <li className='manager-home-box'>
+          <div className='manager-home-box-title'>미처리 보상</div>
+          <ul>
+            <li>보상대상자 이름 / 수험번호 / 감독관이름 / 사유 타입</li>
+            <li>보상대상자 이름 / 수험번호 / 감독관이름 / 사유 타입</li>
+            <li>보상대상자 이름 / 수험번호 / 감독관이름 / 사유 타입</li>
+            <li>보상대상자 이름 / 수험번호 / 감독관이름 / 사유 타입</li>
+          </ul>
         </li>
-        <li className='manager-home-report'>
-
+        <li className='manager-home-box'>
+          <div className='manager-home-box-title'>에러 리포트</div>
+          <ul>
+            <li>제목 / 감독관 이름 / 보고시간</li>
+            <li>제목 / 감독관 이름 / 보고시간</li>
+            <li>제목 / 감독관 이름 / 보고시간</li>
+            <li>제목 / 감독관 이름 / 보고시간</li>
+          </ul>
         </li>
         <li className='manager-home-chart'>
-
+          <div className='manager-home-box-title'>시험 현황</div>
+          <div className='manager-home-chart-select' onClick={()=>setSelectOpen(!selectOpen)}>
+            {chartTitle}
+            <ul className={`manager-home-chart-select-${selectOpen}`}>
+              {
+                chartItem
+              }
+            </ul>
+          </div>
+          <div className='manager-home-chart-box'>
+            <canvas className='manager-home-chart-canvas'></canvas>
+          </div>
         </li>
       </ul>
     </section>
