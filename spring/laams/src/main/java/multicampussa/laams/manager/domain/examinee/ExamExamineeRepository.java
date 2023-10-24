a97f0f9a7fe7da726bb8d4587d3aa4929d5c63a8
Going9 <ickdor123@gmail.com>
2023-10-24 17:23:36 +0900

    Feat: 보상 대상 응시자 상세조회 완성

--- spring/laams/src/main/java/multicampussa/laams/manager/domain/examinee/ExamExamineeRepository.java
+++ spring/laams/src/main/java/multicampussa/laams/manager/domain/examinee/ExamExamineeRepository.java
@@ -20,6 +20,9 @@ public interface ExamExamineeRepository extends JpaRepository<ExamExaminee, Long
     // 시험 응시자 상세 조회
     ExamExaminee findByExamNoAndExamineeNo(Long examNo, Long examineeNo);
 
+    // 응시자 번호로 조회
+    ExamExaminee findByExamineeNo(Long examineeNo);
+
     // 시험 응시자 수 카운트
     @Query(value = "SELECT count(ee) FROM ExamExaminee ee WHERE ee.exam.no = :examNo")
     int countByExamineeNo(Long examNo);
