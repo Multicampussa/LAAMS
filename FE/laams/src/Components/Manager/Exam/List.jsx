4d223358fd2f1af112ea00c0b5ca5d81cef6f5dc
한승현 <vkflek1232@naver.com>
2023-10-26 14:16:16 +0900

    Feat : 시험일정 리스트 UI 완성 - 추가 - 시험일정 리스트 UI - button css 규격 정의 - h1~h5 규격 정의 - 리스트 페이지 CSS 규격 정의 - 변경 - 공통 flex css 변경

--- FE/laams/src/Components/Manager/Exam/List.jsx
+++ FE/laams/src/Components/Manager/Exam/List.jsx
@@ -9,46 +9,56 @@ const List = () => {
     dispatch(setModalShow(true));
   },[dispatch]);
   return (
-    <div className='manager-exam-list'>
-      <div className='manager-exam-list-title'>시험일정</div>
-      <div className='manager-exam-list-search'>
-        <input type='date'/>
-        <select>
-          <option>서울</option>
-        </select>
-        <select>
-          <option>4어쩌구점</option>
-        </select>
+    <section className='list'>
+      <div className='list-title'>
+        <div className='manager-exam-list-title'>
+          <h1>시험일정</h1>
+          <ul className='manager-exam-list-search'>
+            <li><input type='date'/></li>
+            <li>
+              <select>
+                <option>서울</option>
+              </select>
+            </li>
+            <li>
+              <select>
+                <option>4어쩌구점</option>
+              </select>
+            </li>
+          </ul>
+        </div>
+        <button className='manager-exam-list-create' onClick={handleExamCreate}>생성</button>
       </div>
-      <ul className='manager-exam-list-container'>
-      <button className='manager-exam-list-btn-create' onClick={handleExamCreate}>생성</button>
-        <li className='manager-exam-list-box'>
-          <div className='manager-exam-list-item1'>No</div>
-          <div className='manager-exam-list-item2'>시험 명</div>
-          <div className='manager-exam-list-item3'>센터 명</div>
-          <div className='manager-exam-list-item4'>시험 시간</div>
-        </li>
-        <li className='manager-exam-list-box'>
-          <div className='manager-exam-list-item1'>No</div>
-          <div className='manager-exam-list-item2'>시험 명</div>
-          <div className='manager-exam-list-item3'>센터 명</div>
-          <div className='manager-exam-list-item4'>시험 시간</div>
-        </li>
-        <li className='manager-exam-list-box'>
-          <div className='manager-exam-list-item1'>No</div>
-          <div className='manager-exam-list-item2'>시험 명</div>
-          <div className='manager-exam-list-item3'>센터 명</div>
-          <div className='manager-exam-list-item4'>시험 시간</div>
-        </li>
-      </ul>
-      <ul className='manager-exam-list-navigation'>
-        <li>{"<<"}</li>
-        <li>{"<"}</li>
-        <li>1</li>
-        <li>{">"}</li>
-        <li>{">>"}</li>
-      </ul>
-    </div>
+      <div className='list-box'>
+        <ul>
+          <li className='manager-exam-list-item'>
+            <div>No</div>
+            <div>시험 명</div>
+            <div>센터 명</div>
+            <div>시험 시간</div>
+          </li>
+          <li className='manager-exam-list-item'>
+            <div>1</div>
+            <div>SSAPIc</div>
+            <div>구미점</div>
+            <div>9:00-10:00</div>
+          </li>
+          <li className='manager-exam-list-item'>
+            <div>2</div>
+            <div>SSAPIc</div>
+            <div>서울점</div>
+            <div>10:00-11:00</div>
+          </li>
+        </ul>
+        <ul className='flex-row-center gap-1'>
+          <li><button>처음</button></li>
+          <li><button>이전</button></li>
+          <li><button>1</button></li>
+          <li><button>다음</button></li>
+          <li><button>끝</button></li>
+        </ul>
+      </div>
+    </section>
   )
 }
 
