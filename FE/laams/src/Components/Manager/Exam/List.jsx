ec38b9d78f73a490447355a62df1e06628038719
한승현 <vkflek1232@naver.com>
2023-10-27 17:12:14 +0900

    FEAT : 운영자 상세정보 조회 (FIXME : 감독관 리스트)

--- FE/laams/src/Components/Manager/Exam/List.jsx
+++ FE/laams/src/Components/Manager/Exam/List.jsx
@@ -1,28 +1,71 @@
-import React, { useCallback } from 'react'
+import React, { useCallback, useEffect, useMemo, useState } from 'react'
 import { useDispatch } from 'react-redux';
 import { setModalShow, setModalType } from '../../../redux/actions/modalAction';
+import useApi from './../../../Hook/useApi';
+import { setExamNo } from '../../../redux/actions/managerExamDetailAction';
 
 const List = () => {
   const dispatch = useDispatch();
+  const api = useApi();
+  const [date,setDate] = useState(new Date());
+  const [location,setLocation] = useState("서울");
+  const [center,setCenter] = useState("SSAPIc서울SDA센터");
+  const [data,setData] = useState([]);
+
+  const getDateFormat = useCallback(()=>{
+    let month = date.getMonth()+1;
+    return `${date.getFullYear()}-${month < 10 ? "0"+month:month}-${date.getDate()}`;
+  },[date])
+
+  const getExamList = useCallback(async(date)=>{
+    await api.get(`manager/exam/daily?year=${date.getFullYear()}&month=${date.getMonth()+1}&day=${date.getDate()}`)
+    .then(({data})=>setData(data)).catch(err=>console.log(err.response));
+  },[api]);
+
+  const handleExamItem = useCallback((no)=>{
+    dispatch(setExamNo(no));
+    dispatch(setModalType("exam-detail"));
+    dispatch(setModalShow(true));
+  },[dispatch]);
+  
+  //TODO : 시험목록데이터를 불러와 Div를 반환
+  const examItems = useMemo(()=>{
+    return data.map((e,idx)=>{
+      const curDate = new Date(e.examDate);
+      return <li onClick={()=>handleExamItem(e.no)} key={idx} className='manager-exam-list-item'>
+        <div>{e.no}</div>
+        <div>{e.examType} {e.examLanguage}</div>
+        <div>{e.centerName}</div>
+        <div>{curDate.getHours()}:{curDate.getMinutes()}</div>
+      </li>
+    });
+  },[data,handleExamItem])
+
+  //TODO : 날짜가 변경되면 목록을 가져옴
+  useEffect(()=>{
+    getExamList(date);
+  },[date,getExamList])
+
   const handleExamCreate = useCallback(()=>{
     dispatch(setModalType("exam-create"));
     dispatch(setModalShow(true));
   },[dispatch]);
+
   return (
     <section className='list'>
       <div className='list-title'>
         <div className='manager-exam-list-title'>
           <h3>시험일정</h3>
           <ul className='manager-exam-list-search'>
-            <li><input type='date'/></li>
+            <li><input type='date' value={getDateFormat()} onChange={e=>setDate(new Date(e.target.value))} /></li>
             <li>
-              <select>
+              <select onChange={(e)=>setLocation(e.target.value)}>
                 <option>서울</option>
               </select>
             </li>
             <li>
-              <select>
-                <option>4어쩌구점</option>
+              <select onChange={e=>setCenter(e.target.value)}>
+                <option>SSAPIc서울SDA센터</option>
               </select>
             </li>
           </ul>
@@ -37,18 +80,9 @@ const List = () => {
             <div>센터 명</div>
             <div>시험 시간</div>
           </li>
-          <li className='manager-exam-list-item'>
-            <div>1</div>
-            <div>SSAPIc</div>
-            <div>구미점</div>
-            <div>9:00-10:00</div>
-          </li>
-          <li className='manager-exam-list-item'>
-            <div>2</div>
-            <div>SSAPIc</div>
-            <div>서울점</div>
-            <div>10:00-11:00</div>
-          </li>
+          {
+            examItems
+          }
         </ul>
         <ul className='flex-row-center gap-1'>
           <li><button>처음</button></li>
