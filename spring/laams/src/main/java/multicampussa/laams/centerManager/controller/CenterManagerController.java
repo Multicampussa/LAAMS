4e4c2a3a05b627def1d02946b7f802d738e3082c
김현지 <hjkasd12345@naver.com>
2023-11-02 13:27:54 +0900

    Feat: 센터매니저 센터별 시험 시험 리스트 조회

--- spring/laams/src/main/java/multicampussa/laams/centerManager/controller/CenterManagerController.java
+++ spring/laams/src/main/java/multicampussa/laams/centerManager/controller/CenterManagerController.java
@@ -11,15 +11,16 @@ import multicampussa.laams.global.CustomExceptions;
 import multicampussa.laams.home.member.jwt.JwtTokenProvider;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import springfox.documentation.annotations.ApiIgnore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Api(tags = "센터매니저 관련 기능")
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1/centermanager")
 public class CenterManagerController {
 
     private final CenterManagerService centerManagerService;
@@ -27,7 +28,7 @@ public class CenterManagerController {
 
     // 센터 매니저의 감독관 신청 승인
     @ApiOperation(value = "센터 매니저의 감독관 신청 승인")
-    @PutMapping("/api/v1/centermanager/confirm")
+    @PutMapping("/confirm")
     @ApiResponses(value = {
             @io.swagger.annotations.ApiResponse(code = 200, message = "x번 감독관의 y번 시험 승인을 완료했습니다.", response = ConfirmDirectorRequest.class),
             @io.swagger.annotations.ApiResponse(code = 400, message = "잘못된 요청"),
@@ -50,5 +51,29 @@ public class CenterManagerController {
         }
 
     }
+
+    // 센터별 시험 조회
+    @ApiOperation(value = "센터 별 시험 월별 및 일별 조회")
+    @GetMapping("exams")
+    public ResponseEntity<Map<String, Object>> getCenterExamList(@RequestHeader String authorization, @RequestParam int year, @RequestParam int month, @RequestParam(value = "day", defaultValue = "0") int day){
+        Map<String, Object> resultMap = new HashMap<>();
+        try{
+            String token  = authorization.replace("Bearer ", "");
+            String authority = jwtTokenProvider.getAuthority(token);
+            String centerManagerId = jwtTokenProvider.getId(token);
+
+            resultMap.put("message","시험을 성공적으로 조회했습니다.");
+            resultMap.put("data", centerManagerService.getCenterExamList(centerManagerId, year, month, day, authority));
+            resultMap.put("code", HttpStatus.OK.value());
+            resultMap.put("status", "success");
+            return new ResponseEntity<>(resultMap, HttpStatus.OK);
+        }catch (IllegalArgumentException e){
+            resultMap.put("message", e.getMessage());
+            resultMap.put("status", HttpStatus.BAD_REQUEST.value());
+            return new ResponseEntity<>(resultMap, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+   
 
 }
--- spring/laams/src/main/java/multicampussa/laams/centerManager/service/CenterManagerService.java
+++ spring/laams/src/main/java/multicampussa/laams/centerManager/service/CenterManagerService.java
@@ -1,18 +1,29 @@
 package multicampussa.laams.centerManager.service;
 
 import lombok.RequiredArgsConstructor;
+import multicampussa.laams.centerManager.domain.CenterManager;
+import multicampussa.laams.centerManager.domain.CenterManagerRepository;
+import multicampussa.laams.centerManager.dto.CenterExamListDto;
 import multicampussa.laams.centerManager.dto.ConfirmDirectorRequest;
+import multicampussa.laams.director.dto.director.ExamMonthDayListDto;
 import multicampussa.laams.global.CustomExceptions;
+import multicampussa.laams.manager.domain.center.CenterRepository;
+import multicampussa.laams.manager.domain.exam.Exam;
 import multicampussa.laams.manager.domain.exam.ExamDirector;
 import multicampussa.laams.manager.domain.exam.ExamDirectorRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class CenterManagerService {
 
     private final ExamDirectorRepository examDirectorRepository;
+    private final CenterManagerRepository centerManagerRepository;
+    private final CenterRepository centerRepository;
 
     @Transactional
     public void confirmDirector(ConfirmDirectorRequest request) {
@@ -22,5 +33,20 @@ public class CenterManagerService {
             throw new CustomExceptions.ExamDirectorNotFoundException("해당 시험을 신청한 감독관이 없습니다.");
         }
         examDirector.confirmDirector();
+    }
+
+    @Transactional
+    public List<CenterExamListDto> getCenterExamList(String centerManagerId, int year, int month, int day, String authority){
+        if(authority.equals("ROLE_CENTER_MANAGER")){
+            List<CenterExamListDto> centerExamListDtos = new ArrayList<>();
+            List<Exam> exams = centerRepository.findAllByCenterManagerIdContainingMonthAndDay(centerManagerId, year, month, day);
+            for(Exam exam : exams){
+                centerExamListDtos.add(new CenterExamListDto(exam));
+            }
+            return centerExamListDtos;
+        }
+        else {
+            throw new IllegalArgumentException("접근 권한이 없습니다.");
+        }
     }
 }
