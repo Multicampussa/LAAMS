2cfde943c7607ed98b297185219b229f573951d7
yewon830 <wonderwall00@naver.com>
2023-11-03 14:37:27 +0900

    Feat : 감독관 홈 공지사항 박스 api 연동 -공지사항 리스트 (상위 6개) -공지사항 더보기 및 상세보기

--- FE/laams/src/Components/Director/Home/DirectorHome.jsx
+++ FE/laams/src/Components/Director/Home/DirectorHome.jsx
@@ -1,12 +1,16 @@
-import React, { useCallback, useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useMemo, useState } from 'react'
 import Calendar from '../../Common/Calendar/Calendar'
 import useApi from '../../../Hook/useApi';
 import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 const DirectorHome = () => {
 
   const [examList,setExamList] = useState();
   const api = useApi();
+  const navigate = useNavigate();
   const [curDate,setCurDate] = useState(new Date());
+  const [todayExamList, setTodayExamList] = useState([]);
+  const [noticeListData, setNoticeListData] = useState([]);
   const userId = useSelector(state=>state.User.memberId);
   const getDirectorInfo = useCallback(async()=>{
     if(!userId || !api){
@@ -15,6 +19,41 @@ const DirectorHome = () => {
     const response = await api.get(`member/info/${userId}`);
     return response.data.data.memberNo;
   },[api,userId])
+
+  const getTodayExamList = useCallback(async(curDate)=>{
+    const directorUserNo = await getDirectorInfo();
+    await api.get(`director/${directorUserNo}/exams?year=${curDate.getFullYear()}&month=${curDate.getMonth()+1}&day=${curDate.getDate()}`)
+    .then(({data})=>{
+      const res = [];
+      console.log(data)
+      data.data.forEach(e=>{
+        const date = new Date(e.examDate);
+        if(res[date.getDate()]){
+          res[date.getDate()].push({
+            centerName : e.centerName,
+            centerRegion : e.centerRegion,
+            examDate : new Date(e.examDate),
+            examType : e.examType,
+            examLanguage : e.examLanguage
+          });
+        }else{
+          res[date.getDate()]=[{
+            centerName : e.centerName,
+            centerRegion : e.centerRegion,
+            examDate : new Date(e.examDate),
+            examType : e.examType,
+            examLanguage : e.examLanguage
+          }];
+        }
+      });
+      if (res.length>=3){
+        setTodayExamList(res.slice(0,3));
+      }else{
+        setTodayExamList(res)
+      }
+
+    }).catch((err)=>{console.log(err)})
+  },[setTodayExamList,api,getDirectorInfo])
 
   const getExamList = useCallback(async(date)=>{
     const directorUserNo = await getDirectorInfo();
@@ -46,10 +85,16 @@ const DirectorHome = () => {
     }).catch(err=>console.log(err.response));
   },[api,getDirectorInfo]);
 
-  useEffect(()=>{
-    if(!curDate) return;
-    getExamList(curDate);
-  },[getExamList,curDate]);
+  //
+  const showTodayExamList = useMemo(()=>{
+    if(todayExamList.length===0){
+      return <li>오늘의 일정이 없습니다</li>
+    }else{
+      return todayExamList.map((e,index)=>{
+        return <li>[{e.examType}]{e.centerName}({e.centerRegion})</li>
+      })
+    }
+  },[todayExamList])
 
   //TODO : 이전 달 호출
   //FIXME : 데이터 갱신
@@ -79,6 +124,49 @@ const DirectorHome = () => {
     setCurDate(new Date(year,month,1));
   },[curDate])
 
+  const getNotice = useCallback(()=>{
+    api.get('http://k9d101.p.ssafy.io:8080/api/v1/app/notice/list?count=6&page=1')
+    .then(({data})=>{
+      setNoticeListData(data.data)
+      console.log(data.data)
+    })
+    .catch((err)=>{
+      console.log(err)
+    })
+  },[api])
+
+  const showNotice = useMemo(()=>{
+    if(!noticeListData){
+      return <li>공지사항이 없습니다</li>
+    }else{
+      return noticeListData.map((notice, index) => {
+        const createTime = new Date(notice.createdAt);
+        const year = createTime.getFullYear();
+        const month = createTime.getMonth();
+        const day = createTime.getDay();
+        const hours = createTime.getHours() <10? '0'+createTime.getHours():createTime.getHours();
+        const min = createTime.getMinutes() <10? '0'+createTime.getMinutes():createTime.getMinutes(); 
+        
+        return(
+          <li className='director-home-notice-box-items-list'
+          onClick={()=>{
+            navigate(`/notice/detail/${notice.noticeNo}`)
+          }}>
+            <div className='director-home-notice-box-items-list-title'>{notice.title}</div>
+            <div className='director-home-notice-box-items-list-time'>{year}-{month}-{day} {hours}:{min}</div>
+          </li>
+        )
+    })
+  }
+  },[noticeListData,navigate])
+  
+  useEffect(()=>{
+    if(!curDate) return;
+    getExamList(curDate);
+    getTodayExamList(curDate);
+    getNotice();
+  },[getNotice,getExamList,curDate,getTodayExamList]);
+
   return (
     <section className='director-home'>
       <div className='director-home-container'>
@@ -92,16 +180,28 @@ const DirectorHome = () => {
           <div className='director-home-todo-title'>오늘 일정</div>
           <div className='director-home-todo-box'>
             <button className='director-home-todo-box-btn'>감독관 센터 도착 인증</button>
-            <div className='director-home--todo-box-items'></div>
+            <ul className='director-home--todo-box-items'>
+              {
+                showTodayExamList
+              }
+            </ul>
           </div>
         </article>
         <article className='director-home-notice'>
           <div className='director-home-notice-flex'>
             <div className='director-home-notice-flex-title'>공지사항</div>
-            <div className='director-home-notice-flex-additional'>+ 더보기</div>
+            <div className='director-home-notice-flex-additional'
+            onClick={()=>{
+              navigate('/notice')
+            }}
+            >+ 더보기</div>
           </div>
           <div className='director-home-notice-box'>
-            <div className='director-home-notice-box-items'></div> 
+            <ul className='director-home-notice-box-items'>
+            {
+              showNotice
+            }  
+            </ul> 
           </div>
         </article>
         <button className='director-home-btn-hidden'>감독관 센터 도착 인증</button>
