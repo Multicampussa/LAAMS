0f6b31d875914f7afceb5350c03d9e5aefc34a04
Going9 <ickdor123@gmail.com>
2023-11-06 16:07:39 +0900

    Refactor: 시험에 최대 감독관 수 추가

--- spring/laams/src/main/java/multicampussa/laams/manager/service/exam/ExamService.java
+++ spring/laams/src/main/java/multicampussa/laams/manager/service/exam/ExamService.java
@@ -48,7 +48,8 @@ public class ExamService {
         // 입력 받은 매니저 번호로 시험 담당 매니저 호출
         Manager responsibleManager = managerRepository.findById(request.getManagerNo())
                 .orElseThrow(() -> new CustomExceptions.ManagerNotFoundException(request.getManagerNo() + "번 매니저는 존재하지 않습니다."));
-        examRepository.save(new Exam(existingCenter, request.getExamDate(), responsibleManager, request.getRunningTime(), request.getExamType(), request.getExamLanguage()));
+        examRepository.save(new Exam(existingCenter, request.getExamDate(), responsibleManager,
+                request.getRunningTime(), request.getExamType(), request.getExamLanguage(), request.getMaxDirector()));
 
         return ResponseEntity.ok("시험이 성공적으로 생성되었습니다");
     }
@@ -135,7 +136,8 @@ public class ExamService {
         Manager manager = managerRepository.findById(request.getNewManagerNo())
                 .orElseThrow(() -> new CustomExceptions.ManagerNotFoundException(request.getNewManagerNo() + "번 매니저는 존재하지 않습니다."));
 
-        existingExam.updateExamInfo(existingCenter, request.getNewExamDate(), manager, request.getNewRunningTime(), request.getNewExamType());
+        existingExam.updateExamInfo(existingCenter, request.getNewExamDate(), manager, request.getNewRunningTime(),
+                request.getNewExamType(), request.getNewMaxDirector());
     }
 
     // 시험 삭제
