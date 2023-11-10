1c1553f2d5feacb2132f21adde00f09f6a6ca850
한승현 <vkflek1232@naver.com>
2023-11-10 12:33:02 +0900

    Feat : 운영자 보상 승인 / 거절

--- FE/laams/src/Components/Manager/Compensation/Compensation.jsx
+++ FE/laams/src/Components/Manager/Compensation/Compensation.jsx
@@ -1,13 +1,51 @@
 import React, { useEffect } from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import useApi from '../../../Hook/useApi';
+import { useState } from 'react';
+import { useCallback } from 'react';
+import { setModalShow, setModalType } from '../../../redux/actions/modalAction';
+import { setManagerCompensationIsResetList } from '../../../redux/actions/managerCompensationAction';
 
 const ExamCompensation = () => {
   const examineeNo = useSelector(state=>state.ManagerCompensation.examineeNo);
+  const examNo = useSelector(state=>state.ManagerCompensation.examNo);
+  const api = useApi();
+  const [data,setData] = useState();
+  const dispatch = useDispatch();
   useEffect(()=>{
-    if(!examineeNo) {return;}
-    console.log(examineeNo);
-  },[examineeNo]);
-  
+    if(!examineeNo || !api) {return;}
+    api.get(`manager/examinees/compensation/${examineeNo}`)
+      .then(({data})=>setData(data));
+  },[examineeNo,api]);
+  const getDateFormat = useCallback(()=>{
+    if(!data)return;
+    const date = new Date(data.examDate);
+    return `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}`;
+  },[data])
+  const handleConfirm = useCallback(()=>{
+    if(!examNo||!examineeNo||!api) return;
+    api.put("manager/compensation/confirm",{
+      "examNo": examNo,
+      "examineeNo": examineeNo
+    }).then(({data})=>{
+      alert(data.data);
+      dispatch(setManagerCompensationIsResetList());
+      dispatch(setModalType(null));
+      dispatch(setModalShow(false));
+    }).catch(err=>console.log(err));
+  },[examineeNo,examNo,api,dispatch])
+  const handleDeny = useCallback(()=>{
+    if(!examNo||!examineeNo||!api) return;
+    api.put("manager/compensation/deny",{
+      "examNo": examNo,
+      "examineeNo": examineeNo
+    }).then(({data})=>{
+      alert(data.data);
+      dispatch(setManagerCompensationIsResetList());
+      dispatch(setModalType(null));
+      dispatch(setModalShow(false));
+    }).catch(err=>console.log(err));
+  },[examineeNo,examNo,api])
   return (
     <section className='compensation'>
         <div className='compensation-container'>
@@ -16,15 +54,15 @@ const ExamCompensation = () => {
               <div className='flex-row flex-wrap'>
                 <div className='compensation-container-box-row'>
                   <div className='compensation-container-box-title'>보상 사유</div>
-                  <div className='compensation-container-box-item'>네트워크 오류</div>
+                  <div className='compensation-container-box-item'>{data?data.compensationReason:""}</div>
                 </div>
                 <div className='compensation-container-box-row'>
-                  <div className='compensation-container-box-title'>보고자</div>
-                  <div className='compensation-container-box-item'>김감독</div>
+                  <div className='compensation-container-box-title'>시험</div>
+                  <div className='compensation-container-box-item'>{data?data.examType:""}</div>
                 </div>
                 <div className='compensation-container-box-row'>
                   <div className='compensation-container-box-title'>보상 대상</div>
-                  <div className='compensation-container-box-item'>김보상</div>
+                  <div className='compensation-container-box-item'>{data?data.examineeName:""}</div>
                 </div>
                 <div className='compensation-container-box-row'>
                   <div className='compensation-container-box-title'>수험 번호</div>
@@ -32,21 +70,21 @@ const ExamCompensation = () => {
                 </div>
                 <div className='compensation-container-box-row'>
                   <div className='compensation-container-box-title'>장소</div>
-                  <div className='compensation-container-box-item'>서울 SSAPIc센터</div>
+                  <div className='compensation-container-box-item'>{data?`[${data.centerRegion}]${data.centerName}`:""}</div>
                 </div>
                 <div className='compensation-container-box-row'>
                   <div className='compensation-container-box-title'>일시</div>
-                  <div className='compensation-container-box-item'>2023년11월6일</div>
+                  <div className='compensation-container-box-item'>{data?getDateFormat():""}</div>
                 </div>
 
               </div>
 
               <div className='compensation-container-box-title'>내용</div>
-              <textarea readOnly className='compensation-container-box-textarea' value="내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용"/>
+              <textarea readOnly className='compensation-container-box-textarea' value={data&&data.compensationReason?data.compensationReason:""}/>
             </div>
             <div className='flex-row gap-1'>
-              <button className='compensation-btn-cancle'>거절</button>
-              <button className='compensation-btn'>승인</button>
+              <button onClick={handleDeny} className='compensation-btn-cancle'>거절</button>
+              <button onClick={handleConfirm} className='compensation-btn'>승인</button>
             </div>
         </div>        
 
--- FE/laams/src/redux/actions/managerCompensationAction.js
+++ FE/laams/src/redux/actions/managerCompensationAction.js
@@ -2,3 +2,13 @@ export const setManagerCompensationNo = (payload) => ({
   type: "examineeNo",
   payload
 });
+
+export const setManagerCompensationExamNo = (payload) => ({
+  type: "examNo",
+  payload
+});
+
+export const setManagerCompensationIsResetList = (payload) => ({
+  type: "isResetList",
+  payload
+})
