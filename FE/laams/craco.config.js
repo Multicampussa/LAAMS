008108b9666b2d3aa5bbc3a21bbda785c0881805
BOOK-7HF2F58CR4\sour1 <wonderwall00@naver.com>
2024-09-29 23:28:04 +0900

    docs : craco config파일 수정

--- FE/laams/craco.config.js
+++ FE/laams/craco.config.js
@@ -4,9 +4,9 @@ module.exports = {
     webpack: {
         plugins: [
             new CompressionPlugin({
-                filename: '[path].gz[query]', // 압축된 파일 이름
-                algorithm: 'gzip', // Gzip 알고리즘 사용
-                test: /\.(js|css|html|svg)$/, // 압축할 파일 형식
+                filename: '[path][name].gz[query]', // 파일 경로와 이름을 포함
+                algorithm: 'gzip',
+                test: /\.(js|css|html|svg)$/,
                 threshold: 10240, // 압축할 최소 파일 크기 (10KB)
                 minRatio: 0.8, // 최소 압축 비율
             }),
